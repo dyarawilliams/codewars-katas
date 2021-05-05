3b192daa8e9bca3d67ae5f067086258a3e6eb55d
D'yara Williams <dyara.williams@gmail.com>
2021-05-04 22:49:42 -0400

    added details to 8 kyu katas

--- 8-kyu/Shortest Word/index.js
+++ 8-kyu/Shortest Word/index.js
@@ -1,3 +1,7 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
 function findShort(s){
     console.log(s)
     let str = s.split(' ').map(word => word.length)
