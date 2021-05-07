bb9cd06033a340bef8290e9f571c8c1c3eeded36
D'yara Williams <dyara.williams@gmail.com>
2021-05-06 23:57:00 -0400

    added a space

--- 8-kyu/Basic Mathematical Operations/index.js
+++ 8-kyu/Basic Mathematical Operations/index.js
@@ -4,7 +4,8 @@ Your task is to create a function that does four basic mathematical operations.
 The function should take three arguments - operation(string/char), value1(number), value2(number).
 The function should return result of numbers after applying the chosen operation.
 
-Examples: 
+Examples:
+ 
   basicOp('+', 4, 7)         // Output: 11
   basicOp('-', 15, 18)       // Output: -3
   basicOp('*', 5, 5)         // Output: 25
