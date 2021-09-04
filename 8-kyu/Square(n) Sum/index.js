d54b024c508e7fb6dd2183174a399e67500a6419
D'yara Williams <dyara.williams@gmail.com>
2021-09-03 21:24:22 -0400

    remove prep info

--- 8-kyu/Square(n) Sum/index.js
+++ 8-kyu/Square(n) Sum/index.js
@@ -2,13 +2,6 @@
 
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9. */
 
-// PREP
-// Parameters - numbers(integer)
-// Returns - sum of the squared results
-// Example - [1,2] = 5 or [0,3,4,5] = 50
-
-// Psuedo 
-// This function takes in an array of numbers and returns the each number in the array squared sum
 function squareSum(numbers){
     // assign sum to 0
     let sum = 0;
