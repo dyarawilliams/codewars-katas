4ed13d29e16e828a80ca821249450c937df73bec
D'yara Williams <dyara.williams@gmail.com>
2022-06-19 00:45:49 -0400

    add grasshopper create the rooms 8 kyu kata

---
+++ 8-kyu/Grasshopper - Create the rooms/index.js
@@ -0,0 +1,20 @@
+// Escape the room
+// You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+
+let rooms = {
+    room1: {
+        name: "Run",
+        description: "Your fridge is running better catch it",
+        completed: true
+    },
+    room2: {
+        name: "Walk",
+        description: "Take your time",
+        completed: true
+    },
+    room3: {
+        name: "Sprint",
+        description: "Go, Go, Go",
+        completed: false
+    }
+}
