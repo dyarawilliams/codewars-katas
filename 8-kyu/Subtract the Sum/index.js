6dac90ef92b0d3bd260af94b8d16b4b6287dbb99
D'yara Williams <dyara.williams@gmail.com>
2022-12-20 23:43:11 -0500

    add subtract the sum 8 kyu kata

---
+++ 8-kyu/Subtract the Sum/index.js
@@ -0,0 +1,8 @@
+// Parameters: a number(integer)
+// Return: a string, fruit
+// Examples: SubtractSum(10) => "apple", SubtractSum(53) => "apple"
+// Psuedo: Declare a function called SubtractSum that takes in a number(integer) and returns the associated fruit, otherwise return sum of all digits
+
+const SubtractSum = n => 'apple'
+
+console.log(SubtractSum(10))
