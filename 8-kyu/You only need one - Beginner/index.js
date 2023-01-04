c17e5f79564041bbe61fe0760ade4089a9ebf6b0
D'yara Williams <dyara.williams@gmail.com>
2023-01-03 22:33:04 -0500

    add solution to 8 kyu kata

--- 8-kyu/You only need one - Beginner/index.js
+++ 8-kyu/You only need one - Beginner/index.js
@@ -4,6 +4,17 @@
 
 // Return true if the array contains the value, false if not.
 
-function check(a, x) {
-    return a.includes(x)
-}
+// Parameters: two params, a equals array of strings or numbers(integers), and x is the value to be checked
+// Returns: boolean, true if contains the value, x, otherwise false
+// Examples: check([66, 101], 66) => true, check(['t', 'e', 's', 't'], 'e') => true, check(['what', 'a', 'great', 'kata'], 'kat') => false
+
+// Psuedo: Declare a function called check that takes in two parameters and array of strings or numbers
+
+// Solution 1
+// function check(a, x) {
+//     // Check if the array contains the value
+//     return a.includes(x)
+// }
+
+// Solution 2 
+const check = (arr, x) => arr.includes(x)
