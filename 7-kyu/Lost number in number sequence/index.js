73cc31138db57867fe24ecc2bb5e38e074775d15
D'yara Williams <dyara.williams@gmail.com>
2023-01-18 16:30:05 -0500

    add lost number in number sequence 7 kyu kata

---
+++ 7-kyu/Lost number in number sequence/index.js
@@ -0,0 +1,29 @@
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+// Parameters: array of numbers
+// Returns: number(integer)
+// Examples: findDeletedNumber([1,2,3,4,5], [3,4,1,5]); returns 2
+//           findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]); returns 5
+//           findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]); returns 0
+
+// Psudeo: Declare a function called findDeleteNumber that takes in two parameters the first is an aray of numbers and the second is an mixed array of numbers.
+
+const findDeletedNumber = (arr, mixArr) => {
+    // Compare the lengths of both arrays, if equal then assumenothing has been deleted
+    if(arr.length == mixArr.length) return 0
+    // Loop through array and compare the arr to the mixArr, if the number doesn't exist in mixArr then possibly removed from the first return that current value
+    return arr.filter(num => mixArr.indexOf(num) == -1)[0]
+}
+
+console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]))
+console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]))
+console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]))
