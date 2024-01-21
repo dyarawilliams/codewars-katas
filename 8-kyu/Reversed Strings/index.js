bace5e4cb1e05c71a235c01200a72128b3dc8e6a
D'yara Williams <dyara.williams@gmail.com>
2024-01-21 16:41:30 -0500

    add arrow function solution for 8 kyu kata

--- 8-kyu/Reversed Strings/index.js
+++ 8-kyu/Reversed Strings/index.js
@@ -1,6 +1,8 @@
 // Complete the solution so that it reverses the string passed into it.
 // 'world => 'dlrow'
 
-function solution(str){
-    return str.split('').reverse().join('')
-}
+// function solution(str){
+//     return str.split('').reverse().join('')
+// }
+
+const solution = str => [...str].reverse().join('')
