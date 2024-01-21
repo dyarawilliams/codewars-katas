57c695c924c6c4c1278dddf074a83bfc6d23e01d
D'yara Williams <dyara.williams@gmail.com>
2024-01-21 18:14:04 -0500

    add todays solution and console logs

--- 8-kyu/Grasshopper - Summation/index.js
+++ 8-kyu/Grasshopper - Summation/index.js
@@ -9,11 +9,23 @@
 // summation(8) -> 36
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
-var summation = function (num) {
-    let sum = 0
-        for(let i = 1; i <= num; i++){
-            sum += i
-            console.log(`Count: ${i} and Sum: ${sum}`)
-        }
-    return sum
+// var summation = function (num) {
+//     let sum = 0
+//         for(let i = 1; i <= num; i++){
+//             sum += i
+//             console.log(`Count: ${i} and Sum: ${sum}`)
+//         }
+//     return sum
+// }
+
+var summation = num => {
+    let running_total = 0;
+    for(let i = 1; i <= num; i++){
+        running_total += i
+    }
+    return running_total;
 }
+
+console.log(summation(2))
+console.log(summation(8))
+console.log(summation(1))
