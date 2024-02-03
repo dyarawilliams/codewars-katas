4cfb9a72d625fac03d81fa60e585e77ebce375c7
D'yara Williams <dyara.williams@gmail.com>
2024-02-03 18:19:00 -0500

    add 8kyu kata remove first and last character

--- 8-kyu/Remove First and Last Character/index.js
+++ 8-kyu/Remove First and Last Character/index.js
@@ -1,14 +1,29 @@
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
-function removeChar(str){
-    str = str.split(''); // divides string into an ordered list
-    let removeFirst = str.shift();
-    let removeLast = str.pop();
-    return str.join('');
-}
+// Parameters: string(str) with atleast 2 characters
+// Returns: same string without the first and last characters
+// Example: removeChar('eloquent') -> 'loquen' removeChar('country') -> 'ountr', removeChar('place') -> 'lac'
 
-// OR 
+// Psuedo: Create a function called removeChar that takes in one parameter (a string of two or more characters - named str) and removes the first and last characters from that string.
 
+
+// Solution 1
+// function removeChar(str){
+//     str = str.split(''); // divides string into an ordered list
+//     let removeFirst = str.shift();
+//     let removeLast = str.pop();
+//     return str.join('');
+// }
+
+// Solution 2
 // function removeChar(str){
 //     return str.slice(1, -1)
 // }
+
+// Solution 3 (Arrow Function)
+// Use slice method to extract the first and last index of the string and returns a shallow copy of the remaining characters
+const removeChar = str => str.slice(1, -1)
+
+console.log(removeChar('eloquent'))
+console.log(removeChar('country'))
+console.log(removeChar('place'))
