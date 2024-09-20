32b925c7ed4e2ba7cbde4cee1cda28bd93a571f1
D'yara Williams <dyara.williams@gmail.com>
2024-09-19 22:43:42 -0400

    remove whitespace

--- 8-kyu/Grasshopper - Debug/index.test.js
+++ 8-kyu/Grasshopper - Debug/index.test.js
@@ -1,6 +1,6 @@
 const weatherInfo = require('./index');
 
-test ('celsius converter', () => {
+test('celsius converter', () => {
     expect(weatherInfo(50)).toBe('10 is above freezing temperature');
     expect(weatherInfo(23)).toBe('-5 is freezing temperature');
 })
